b49566560067cb65e8a02be92e991f64d678ca1b
zraccoon <jing.zdev3@gmail.com>
2021-06-06 18:13:08 +1000

    update

--- src/components/Lottery/Round.js
+++ src/components/Lottery/Round.js
@@ -1,7 +1,23 @@
 import React, { useState } from 'react';
+import BigNumber from 'bignumber.js'
+import { useWeb3React } from "@web3-react/core"
+import useWeb3 from "../../hooks/useWeb3";
+import {
+    fetchLotteryInfo
+} from '../../utils/fetchLotteryData'
 
 const Round = () => {
     const [inputNumber, setInputNumber] = useState(1)
+    const [lotteryInfo, setLotteryInfo] = useState(null)
+    
+    const web3 = useWeb3()
+    const { chainId } = useWeb3React()
+
+    const SearchHandler = async () => {
+        const data = await fetchLotteryInfo(web3, chainId, inputNumber)
+        console.log(data)
+        setLotteryInfo(data)
+    }
 
     return (<div className="winner">
         <div className="search-wrap">
@@ -10,21 +26,21 @@ const Round = () => {
                 <div className="input-box">
                     <input type="number" max="100" min="1" step="1" placeholder="5" value={inputNumber} onChange={(e) => setInputNumber(e.currentTarget.value)} />
                 </div>
-                <div className="search-btn">Search</div>
+                <div className="search-btn" onClick={() => SearchHandler()}>Search</div>
             </div>
         </div>
 
         <div className="round-wrap">
-            <p className="round-title">Round #7</p>
+            <p className="round-title">Round #{lotteryInfo ? lotteryInfo.lotteryID : ''}</p>
             <div className="prize-wrap">
                 <div className="win-numbers">
                     <p className="w-title">Winning Numbers</p>
-                    <p className="w-numbers"> 2, 3, 9</p>
+                    <p className="w-numbers">{lotteryInfo ? lotteryInfo.winningNumbers.join(' , ') : ''}</p>
                 </div>
 
                 <div className="win-numbers">
                     <p className="w-title">Total Prizes </p>
-                    <p className="w-numbers">55.46 LQDR</p>
+                    <p className="w-numbers">{lotteryInfo ? new BigNumber(lotteryInfo.prizePoolInLqdr).div(10 ** 18).toFormat(2) : ''} LQDR</p>
                 </div>
 
 
@@ -36,14 +52,14 @@ const Round = () => {
                 <p className="m-title" style={{ width: "200px", textAlign: "left" }}>No. Matched</p>
                 <p className="m-title">Winners</p>
                 <p className="m-title">Prize Pot</p>
-                <p>4</p><p>0</p><p>27.73</p>
-                <p>3</p><p>0</p><p>11.25</p>
-                <p>2</p><p>0</p><p>7.13</p>
+                <p>4</p><p>{lotteryInfo ? lotteryInfo.winnerCounts[3] : 0}</p><p>{lotteryInfo && lotteryInfo.prizeDistribution[3] > 0 ? new BigNumber(lotteryInfo.prizePoolInLqdr).div(10 ** 18).times(lotteryInfo.prizeDistribution[3]).div(100).toFormat(2) : 0}</p>
+                <p>3</p><p>{lotteryInfo ? lotteryInfo.winnerCounts[2] : 0}</p><p>{lotteryInfo && lotteryInfo.prizeDistribution[2] > 0 ? new BigNumber(lotteryInfo.prizePoolInLqdr).div(10 ** 18).times(lotteryInfo.prizeDistribution[2]).div(100).toFormat(2) : 0}</p>
+                <p>2</p><p>{lotteryInfo ? lotteryInfo.winnerCounts[1] : 0}</p><p>{lotteryInfo && lotteryInfo.prizeDistribution[1] > 0 ? new BigNumber(lotteryInfo.prizePoolInLqdr).div(10 ** 18).times(lotteryInfo.prizeDistribution[1]).div(100).toFormat(2) : 0}</p>
             </div>
 
             <div className="burned">
                 <span className="b-title">Burned : </span>
-                <span> 11.09</span>
+                <span>{lotteryInfo && lotteryInfo.prizeDistribution[0] > 0 ? new BigNumber(lotteryInfo.prizePoolInLqdr).div(10 ** 18).times(lotteryInfo.prizeDistribution[0]).div(100).toFormat(2) : 0}</span>
             </div>
         </div>
 
