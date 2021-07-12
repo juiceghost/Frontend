e5b93993e492cd683ccb3371788d5ce74e7ff557
Mohammad Ali <maliabr75@gmail.com>
2021-07-12 21:11:39 +0430

    Add UA

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga"
 
 function init() {
-    const GOOGLE_ANALYTICS_ID = "G-GEZMNMHMJW"
+    const GOOGLE_ANALYTICS_ID = "UA-201902194-1"
     if (typeof GOOGLE_ANALYTICS_ID === 'string') {
         ReactGA.initialize(GOOGLE_ANALYTICS_ID, {
             gaOptions: {
@@ -23,8 +23,10 @@ function sendPageview(path) {
     ReactGA.pageview(path)
 }
 
-export default {
+const GA = {
     init,
     sendEvent,
     sendPageview,
 }
+
+export default GA
