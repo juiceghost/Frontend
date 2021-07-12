07ac0e40cdc7aca3b1665143c94f8a73031f362b
Mohammad Ali <maliabr75@gmail.com>
2021-07-12 21:10:57 +0430

    add default address to ftm

--- src/utils/addressHelpers.js
+++ src/utils/addressHelpers.js
@@ -1,41 +1,41 @@
 import addresses from '../config/constants/contracts'
 
-export const getMasterChefAddress = (chainId = 3) => {
+export const getMasterChefAddress = (chainId = 250) => {
   return addresses.MasterChef[chainId]
 }
 
-export const getSushiAddress = (chainId = 3) => {
+export const getSushiAddress = (chainId = 250) => {
   return addresses.Sushi[chainId]
 }
 
-export const getMulticallAddress = (chainId = 3) => {
+export const getMulticallAddress = (chainId = 250) => {
   return addresses.MulltiCall[chainId]
 }
 
-export const getLotteryAddress = (chainId = 3) => {
+export const getLotteryAddress = (chainId = 250) => {
   return addresses.lottery[chainId]
 }
 
-export const getLotteryNftAddress = (chainId = 3) => {
+export const getLotteryNftAddress = (chainId = 250) => {
   return addresses.lotteryNFT[chainId]
 }
 
-export const getLqdrAddress = (chainId = 3) => {
+export const getLqdrAddress = (chainId = 250) => {
   return addresses.LQDR[chainId]
 }
 
-export const getXLqdrAddress = (chainId = 3) => {
+export const getXLqdrAddress = (chainId = 250) => {
   return addresses.xlqdr[chainId]
 }
 
-export const getFeeDistributorAddress = (chainId = 3) => {
+export const getFeeDistributorAddress = (chainId = 250) => {
   return addresses.feeDistributor[chainId]
 }
 
-export const getFTMDistributorAddress = (chainId = 3) => {
+export const getFTMDistributorAddress = (chainId = 250) => {
   return addresses.ftmDistributor[chainId]
 }
 
-export const getWftmAddress = (chainId = 3) => {
+export const getWftmAddress = (chainId = 250) => {
   return addresses.wftm[chainId]
 }
